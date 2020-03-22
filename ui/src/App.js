30b8dc0762e44c2c4e9b6bb0bf0a1692a1a99cae
Ajay Sankar <ajay0196@gmail.com>
2020-03-21 20:23:55 -0700

    Fixed "react/pref er-stateless-function" lint

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 import ApolloClient from "apollo-boost"
 import { ApolloProvider } from "react-apollo"
 import ProductList from "./ProductList"
@@ -9,19 +9,17 @@ const client = new ApolloClient({
   uri: "http://localhost:3000/graphql",
 })
 
-class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <div>
-          <h1> My Company Inventory </h1>
-          <h3> Showing all available products </h3>
-          <hr />
-          <ProductList />
-        </div>
-      </ApolloProvider>
-    )
-  }
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h1> My Company Inventory </h1>
+        <h3> Showing all available products </h3>
+        <hr />
+        <ProductList />
+      </div>
+    </ApolloProvider>
+  )
 }
 
 export default App
